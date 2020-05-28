e6abbd08f5355e4056028c67b71df0fa03db83e5
v_shtongliu <v_shtongliu@tencent.com>
2020-05-28 20:15:30 +0800

    完成详情

--- pages/_more.vue
+++ pages/_more.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>more</div>
+    <div class="container more-container">
+        <div class="search">
+            <input
+                type="text"
+                class="search-input"
+            >
+            <div class="search-icon">
+                <i class="iconfont icon-search"></i>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
